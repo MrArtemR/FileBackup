57367b8f3d05ce547f9eef3f394a878a0e26e561
MrArtemR <artem.rekhin@glowbyteconsulting.com>
2020-11-06 19:16:31 +0300

    logger configuration

---
+++ FileBackup.Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FileBackup.Models
+{
+    public class AppSettings
+    {
+        public string TargetDirectory { get; set; }
+        public IEnumerable<string> SourceDirectory { get; set; }
+        public IEnumerable<string> Exceptions { get; set; }
+        public bool Archive { get; set; }
+        public string LogLevel { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using FileBackup.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace FileBackup
 {
@@ -6,6 +9,26 @@ namespace FileBackup
     {
         static void Main(string[] args)
         {
+            var configuration = new ConfigurationBuilder()
+               .AddCommandLine(args)
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+               .Build();
+            var setting = configuration.Get<AppSettings>();
+
+            var loglevel = LogLevel.Information;
+            if(!string.IsNullOrEmpty(setting.LogLevel))
+            {
+                loglevel = (LogLevel)Enum.Parse(typeof(LogLevel), setting.LogLevel); ;
+            }
+
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole();
+                builder.SetMinimumLevel(loglevel);
+            });
+            ILogger logger = loggerFactory.CreateLogger<Program>();
+
+
             Console.WriteLine("Hello World!");
         }
     }
